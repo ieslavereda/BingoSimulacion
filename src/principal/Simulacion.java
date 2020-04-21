ed838333b08ba07ffe8c68f7a1bedc57697eaf7d
Joaquin Vicente <Joaquin Vicente@DESKTOP-LPKOK7O>
2020-04-21 18:03:46 +0200

    a

---
+++ src/principal/Simulacion.java
@@ -0,0 +1,15 @@
+package principal;
+
+import controladores.Controlador;
+import view.JFSimulador;
+
+public class Simulacion {
+
+	public static void main(String[] args) {
+
+		Controlador controlador = new Controlador(new JFSimulador());
+		controlador.start();
+
+	}
+
+}
